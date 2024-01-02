d53cbf39e67d9f8508ff2b0c7f9e5199c1ab73a3
Yuri Lopukhin <yurilopukhinfol@mail.ru>
2024-01-02 12:22:09 +0300

    Реализована страница регистрации

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,9 +15,6 @@ function App() {
           <Route path={AppRoute.Registration} element={<RegistrationPage />} />
           <Route path={AppRoute.About} element={<AboutPage />} />
           <Route path={AppRoute.NotFound} element={<NotFoundPage />} />
-
-          {/* <Route path="/catalog" element={<CatalogPage />}></Route>
-          <Route path="/catalog/:id" element={<UserCardPage />}></Route> */}
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/RegistrationPage/RegistrationPage.js
+++ src/pages/RegistrationPage/RegistrationPage.js
@@ -1,6 +1,153 @@
 import React from 'react';
+import { Button, Form, Input } from 'antd';
 import s from './registration__page.module.css';
 
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 8,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 16,
+    },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
 export const RegistrationPage = () => {
-  return <div className={s.registration}>Это страница регистрации.</div>;
+  const [form] = Form.useForm();
+  const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+    <div className={s.registration}>
+      <h2>Регистрация</h2>
+      <Form
+        {...formItemLayout}
+        form={form}
+        name="register"
+        onFinish={onFinish}
+        style={{
+          maxWidth: 500,
+          width: '100%',
+        }}
+        scrollToFirstError
+      >
+        <Form.Item
+          name="login"
+          label="Login"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your login!',
+              // whitespace: true,
+            },
+            {
+              validator: (_, value) => {
+                if (value === '') {
+                  return Promise.reject();
+                } else if (/^[a-zA-Z0-9]+$/.test(value)) {
+                  return Promise.resolve();
+                } else {
+                  return Promise.reject('Login should include numbers and latin letters only');
+                }
+              },
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          name="email"
+          label="E-mail"
+          rules={[
+            {
+              type: 'email',
+              message: 'The input is not valid E-mail!',
+            },
+            {
+              required: true,
+              message: 'Please input your E-mail!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          label="Password"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!',
+            },
+            {
+              validator: (_, value) => {
+                if (value === '') {
+                  return Promise.reject();
+                } else if (/^[a-z]+$/.test(value)) {
+                  return Promise.resolve();
+                } else {
+                  return Promise.reject('Password should include low latin letters only');
+                }
+              },
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          name="confirm"
+          label="Confirm Password"
+          dependencies={['password']}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: 'Please confirm your password!',
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue('password') === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(new Error('The new password that you entered do not match!'));
+              },
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit">
+            Зарегистрироваться
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
